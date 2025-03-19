1c49a335f0ac868e1023a3243a9462ac9bbafa63
toni998 <agolemac3@gmail.com>
2025-03-19 13:35:10 +0100

    refactor : added validation in update score for VAR intervention

--- src/main/java/org/live_odd_service/LiveScoreboard.java
+++ src/main/java/org/live_odd_service/LiveScoreboard.java
@@ -20,10 +20,20 @@ public class LiveScoreboard {
     }
 
     public void updateFootballMatchScore(FootballMatch match, int homeTeamScore, int awayTeamScore) {
-        if (!footballMatches.contains(match) || homeTeamScore < 0 || awayTeamScore < 0) {
-            throw new IllegalArgumentException("Match update not possible, check football match or scores");
+        if (!footballMatches.contains(match)) {
+            throw new IllegalArgumentException("Match update not possible, invalid match");
         }
-        match.updateScore(homeTeamScore, awayTeamScore);
+
+        if (homeTeamScore == -1 && awayTeamScore >= 0 || homeTeamScore >=0 && awayTeamScore == -1) {
+            if (match.getHomeTeamScore() + homeTeamScore < 0 || match.getAwayTeamScore() + awayTeamScore < 0) {
+                throw new IllegalArgumentException("Invalid score update, score cannot be negative after update");
+            }
+            match.updateScore(match.getHomeTeamScore() + homeTeamScore, match.getAwayTeamScore() + awayTeamScore);
+        } else if (homeTeamScore < -1 || awayTeamScore < -1) {
+            throw new IllegalArgumentException("Invalid score update, score cannot be negative after update");
+        } else {
+            match.updateScore(homeTeamScore, awayTeamScore);
+        }
     }
 
     public void finishFootballMatch(FootballMatch match) {
